0a4c45bf8311738dcbdf8eb25c0d8b8a1438731f
Raghav-Pal <automation.devops@gmail.com>
2019-09-10 14:55:56 +0000

    updated tests

--- tests/calculator.js
+++ tests/calculator.js
@@ -18,20 +18,6 @@ describe('demo calculator tests', function () {
 
     });
 
-    it('addition test', function () {
-
-        homepage.get('http://juliemr.github.io/protractor-demo/');
-
-        homepage.enterFirstNumber('4');
-
-        homepage.enterSecondNumber('3');
-
-        homepage.clickGo1();
-
-        homepage.verifyResult('7');
-
-        browser.sleep(2000)
-
-    });
+   
 
 });
