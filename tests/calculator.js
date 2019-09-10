26125b55bc8d678ee02f77612fc8031fb418c565
Raghav-Pal <automation.devops@gmail.com>
2019-09-10 15:30:29 +0000

    updated calculator.js

--- tests/calculator.js
+++ tests/calculator.js
@@ -18,6 +18,22 @@ describe('demo calculator tests', function () {
 
     });
 
+    it('subtraction test', function () {
+
+        homepage.get('http://juliemr.github.io/protractor-demo/');
+
+        homepage.enterFirstNumber('4');
+
+        homepage.enterSecondNumber('3');
+
+        homepage.clickGo1();
+
+        homepage.verifyResult('7');
+
+        browser.sleep(2000)
+
+    });
+
    
 
 });
